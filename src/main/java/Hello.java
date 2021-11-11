c374c469bcd1c6b0420edad3a3abf50fbed2c4b7
Chavez-Noe <noe_fam_chavez@yahoo.com.mx>
2021-11-10 21:05:53 -0600

    Concept Observable.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,8 @@
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.Consumer;
 
-import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hello {
 
@@ -22,6 +22,32 @@ public class Hello {
        Observable.just("Mediante una expresiómn lambda --> Hello World / Hola Mundo") // creando el observable.
                 .subscribe(s -> {System.out.println(s);} // suscribción de la acción.
                 );
+
+       List<Task> tasks = new ArrayList<>(2);
+       tasks.add(new Task("Tarea I", "RX-JAVA"));
+       tasks.add(new Task("Tarea II", "Coroutine whit Kotlin"));
+
+        List<Task> tasksOther = new ArrayList<>(2);
+        tasks.add(new Task("Tarea I - Other", "RX-JAVA"));
+        tasks.add(new Task("Tarea II - Other", "Coroutine whit Kotlin"));
+
+       // Se crea el Observable.
+       Observable<List<Task>> observableTasks = Observable.just(tasks, tasksOther);
+       // suscribir al observable.
+        observableTasks.subscribe(
+                (listTask) -> {
+                    for (Task task : listTask) System.out.println(task);
+                }
+        );
+
+        Observable<Integer> integerObservable = Observable.just(1, 5, 8);
+        integerObservable.subscribe(
+                (a) -> {
+                    System.out.println(a);
+                }
+        );
+
+
     }
 
 }
